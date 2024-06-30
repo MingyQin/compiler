f8aa0d14a97f0e65255b727b1fd66882966521c5
Ming-Yang Qin <mingqin22@gmail.com>
2024-06-30 21:07:30 +0000

    Worked on implemementing unaryOps

--- compiler/test.c
+++ compiler/test.c
@@ -3,42 +3,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include "types.h"
 
-
-typedef struct {
-    int value;
-} expression;
-
-typedef struct {
-    expression *exp;
-} statement;
-
-typedef struct {
-    char *id;
-    statement *statement;
-} function;
-
-typedef struct {
-    function *func;
-} program;
 
 int main(int argc, char *argv[])
 {
-    program ast;
-    // Create enough size for 2 functions
-    ast.func = malloc(sizeof(function) *2);
-
-    // Access the actual memory stored at the locations
-    ast.func[0].id = "main";
-    ast.func[1].id = "fail";
-
-    // Make pointer f point to the location of the function stored at index 1 of the ast.func
-    function *f = &ast.func[1];
-    // Change the value of ast.func[1] to be main
-    f->id = "main";
-
     // Print the change
-    printf("%s\n", ast.func[1].id);
+    unaryOp *unOp = malloc(sizeof(unaryOp));
+    unOp->innerExp = malloc(sizeof(expression));
+    expression *exp = unOp->innerExp;
+    exp->type = CONST;
+    exp->value = 1;
+    exp->unOp = NULL;
 
-    free(ast.func);
+    printf("%d, %d\n", exp->type, exp->value);
+
+    free(exp);
+    free(unOp);
+
 }
